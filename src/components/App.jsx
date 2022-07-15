55af17bc715a879aa4c882121a94d2c270ab1202
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-15 14:15:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,16 +15,12 @@ class App extends Component {
   addContact = ({ name, number }) => {
     const normalizedName = name.toLowerCase();
 
-    let isAdded = false;
-
-    this.state.contacts.forEach(el => {
-      if (el.name.toLowerCase() === normalizedName) {
-        alert(`${name} is already in contacts`);
-        isAdded = true;
-      }
-    });
+    let isAdded = this.state.contacts.find(
+      el => el.name.toLowerCase() === normalizedName
+    );
 
     if (isAdded) {
+      alert(`${name} is already in contacts`);
       return;
     }
 
